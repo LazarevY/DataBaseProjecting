f62445f6f5d0978786cf3d33993f5e52ef436763
LazarevY <lazarev@cs.vsu.ru>
2020-10-11 20:38:19 +0300

    Add insert script

---
+++ insert.sql
@@ -0,0 +1,22 @@
+insert into rent_statuses (status_name) values ('in_force');
+insert into rent_statuses (status_name) values ('violation');
+insert into rent_statuses (status_name) values ('canceled');
+insert into rent_statuses (status_name) values ('completed_successfully');
+
+insert into drive_categories (category_name) values ('B');
+insert into drive_categories (category_name) values ('D');
+
+insert into ratings (rating) values ('neutral');
+insert into ratings (rating) values ('below_average');
+insert into ratings (rating) values ('above_average');
+insert into ratings (rating) values ('high');
+insert into ratings (rating) values ('low');
+
+insert into autos_brands (brand_name) values ('Ford');
+insert into autos_brands (brand_name) values ('Kia');
+insert into autos_brands (brand_name) values ('Datsun');
+insert into autos_brands (brand_name) values ('Renaut');
+insert into autos_brands (brand_name) values ('Lada');
+insert into autos_brands (brand_name) values ('Volkswagen');
+insert into autos_brands (brand_name) values ('Opel');
+insert into autos_brands (brand_name) values ('Chevrolet');
